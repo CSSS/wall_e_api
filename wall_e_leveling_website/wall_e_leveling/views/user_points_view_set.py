04dd34985ac477f8222e7b63d908cb250509452d
Jace Manshadi <jmanshad@protonmail.com>
2023-10-18 19:11:20 -0700

    removing port which gets added on cause of nginx

--- wall_e_leveling_website/wall_e_leveling/views/user_points_view_set.py
+++ wall_e_leveling_website/wall_e_leveling/views/user_points_view_set.py
@@ -16,10 +16,12 @@ class StandardResultsSetPagination(PageNumberPagination):
     max_page_size = 1000
 
     def get_paginated_response(self, data):
+        next_link = self.get_next_link().replace(":443", "")
+        previous_link = self.get_previous_link().replace(":443", "")
         return Response({
             'links': {
-               'next': self.get_next_link(),
-               'previous': self.get_previous_link()
+               'next': next_link,
+               'previous': previous_link
             },
             'count': self.page.paginator.count,
             'total_number_of_pages': self.page.paginator.num_pages,
