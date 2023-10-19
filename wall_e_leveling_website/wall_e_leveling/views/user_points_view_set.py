2ade540f8c286ca859db9148803262e803278d2f
Jace Manshadi <jmanshad@protonmail.com>
2023-10-18 20:51:01 -0700

    fixed nginx

--- wall_e_leveling_website/wall_e_leveling/views/user_points_view_set.py
+++ wall_e_leveling_website/wall_e_leveling/views/user_points_view_set.py
@@ -16,12 +16,10 @@ class StandardResultsSetPagination(PageNumberPagination):
     max_page_size = 1000
 
     def get_paginated_response(self, data):
-        next_link = self.get_next_link().replace(":443", "") if self.get_next_link() is not None else None
-        previous_link = self.get_previous_link().replace(":443", "") if self.get_previous_link() is not None else None
         return Response({
             'links': {
-               'next': next_link,
-               'previous': previous_link
+               'next': self.get_next_link(),
+               'previous': self.get_previous_link()
             },
             'count': self.page.paginator.count,
             'total_number_of_pages': self.page.paginator.num_pages,
