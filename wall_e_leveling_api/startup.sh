1cae76e230247ea7bbc85324bcbec9b65df8cca8
Jace Manshadi <jaymanshad@proton.me>
2024-10-27 11:35:24 -0700

    switching to submodules format

--- wall_e_leveling_api/startup.sh
+++ wall_e_leveling_api/startup.sh
@@ -7,6 +7,7 @@ apk update && apk add gcc
 apk update && apk add musl-dev
 
 pip install --no-cache-dir -r requirements.txt
+pip install --no-cache-dir -r wall_e_models_requirements.txt
 
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
