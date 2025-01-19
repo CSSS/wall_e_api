edc4dba6d54a69f595af42a3d97b52755a3e81d8
Jace Manshadi <jaymanshad@proton.me>
2025-01-19 15:54:51 -0800

    adding query param needed for https://github.com/CSSS/csss-site-backend/issues/96

--- wall_e_leveling_api/wall_e_leveling/views/user_points_view_set.py
+++ wall_e_leveling_api/wall_e_leveling/views/user_points_view_set.py
@@ -1,8 +1,10 @@
+from django.db.models import Q
 from rest_framework import serializers, viewsets
 from rest_framework.response import Response
-from wall_e_models.models import UserPoint, Level
 
 from wall_e_leveling.views.pagination import StandardResultsSetPagination
+from wall_e_models.customFields import pstdatetime
+from wall_e_models.models import UserPoint, Level
 
 
 class UserPointSerializer(serializers.ModelSerializer):
@@ -31,7 +33,7 @@ class UserPointSerializer(serializers.ModelSerializer):
         model = UserPoint
         fields = [
             'name', 'avatar', 'points', 'level_number', 'message_count', 'level_up_specific_points',
-            'points_needed_to_level_up'
+            'points_needed_to_level_up', 'last_updated_date'
         ]
 
 
@@ -45,3 +47,29 @@ class UserPointViewSet(viewsets.ModelViewSet):
 
     def update(self, request, *args, **kwargs):
         return Response("not yet implemented")
+
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+        query = None
+        timestamp = request.query_params.get('timestamp', None)
+        include_null = request.query_params.get('include_null', None)
+        include_null = include_null.lower() == 'true' if include_null else False
+        if timestamp is not None and timestamp.isdigit():
+            timestamp = pstdatetime.from_epoch(int(timestamp))
+            query = Q(last_updated_date__gte=timestamp)
+        if  include_null:
+            if query:
+                query = query | Q(last_updated_date__isnull=True)
+            else:
+                query = Q(last_updated_date__isnull=True)
+        if query:
+            queryset = queryset.filter(query)
+
+
+        page = self.paginate_queryset(queryset)
+        if page is not None:
+            serializer = self.get_serializer(page, many=True)
+            return self.get_paginated_response(serializer.data)
+
+        serializer = self.get_serializer(queryset, many=True)
+        return Response(serializer.data)
